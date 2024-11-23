0a4d902f7f26951cf58f293fbaa862d3146d78b4
Lilly Helbling <lilly@helbling.uk>
2024-11-23 17:09:53 +0000

    Extract script to set up EBS volume

---
+++ scripts/ebs-mount.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Set up
+while [ ! -e /dev/nvme1n1 ]; do
+  echo "Waiting for EBS volume to be attached..."
+  sleep 10
+done
+
+if sudo stat "/mnt/pocketbase" >/dev/null 2>&1; then
+  echo "Folder exists."
+else
+    echo "Folder does not exist. New root file system"
+    sudo mkdir /mnt/pocketbase
+    # Configure the EBS volume to be automatically mounted
+    echo "/dev/nvme1n1  /mnt/pocketbase  ext4  defaults,nofail  0  2" | sudo tee -a /etc/fstab
+fi
+
+if ! sudo blkid /dev/nvme1n1; then
+  # EBS has no file system - create one
+  sudo mkfs -t ext4 /dev/nvme1n1
+fi
+
+sudo mount /dev/nvme1n1 /mnt/pocketbase
